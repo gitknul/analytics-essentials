f63614afac5753c9954117ce26325624f6e9bab1
Sunny Freshheads <s-feijen@hotmail.nl>
2023-05-25 11:05:13 +0200

    Add load tag manager component

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,12 @@ import { pushTrackingEvent } from './GA4';
 import { pushDataLayer } from './tagManager';
 import { EventTypes } from './types';
 import { LoadGA4 } from './components/LoadGA4';
+import { LoadTagmanager } from './components/LoadTagmanager';
 
-export { pushTrackingEvent, pushDataLayer, EventTypes, LoadGA4 };
+export {
+    pushTrackingEvent,
+    pushDataLayer,
+    EventTypes,
+    LoadGA4,
+    LoadTagmanager,
+};
